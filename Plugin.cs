29ca974a7620d49e91213a1059be4cb7c41918bc
HanSolo1000Falcon <202703912+HanSolo1OOOFalcon@users.noreply.github.com>
2025-04-07 18:18:40 +0200

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,23 +1,41 @@
-﻿using System;
+using System;
 using BepInEx;
+using BepInEx.Configuration;
 using Photon.Pun;
 using UnityEngine;
 using Utilla;
+using static WhoIsThatMonke.PublicVariablesGatherHere;
 
 namespace WhoIsThatMonke
 {
+    // This is your mod's main class.
     [BepInIncompatibility("com.hansolo1000falcon.gorillatag.whatisthefps")]
     [BepInIncompatibility("com.hansolo1000falcon.gorillatag.whoischeating")]
     [BepInIncompatibility("com.hansolo1000falcon.gorillatag.whoisspeeding")]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
-    public class Plugin : BaseUnityPlugin
+	public class Plugin : BaseUnityPlugin
     {
+        private ConfigEntry<bool> PlatformCheckerEnabled;
+        private ConfigEntry<bool> VelocityCheckerEnabled;
+        private ConfigEntry<bool> FPSCheckerEnabled;
+        private ConfigEntry<bool> ColorCodeSpooferEnabled;
+
         void Start()
         {
             HarmonyPatches.ApplyHarmonyPatches();
             ExitGames.Client.Photon.Hashtable properties = new ExitGames.Client.Photon.Hashtable();
             properties.Add("cheese is gouda", PluginInfo.Name);
             PhotonNetwork.LocalPlayer.SetCustomProperties(properties);
+
+            PlatformCheckerEnabled = Config.Bind("Settings", "Platform Checker", true, "Enable or disable the platform checker.");
+            VelocityCheckerEnabled = Config.Bind("Settings", "Velocity Checker", true, "Enable or disable the velocity checker.");
+            FPSCheckerEnabled = Config.Bind("Settings", "FPS Checker", true, "Enable or disable the FPS checker.");
+            ColorCodeSpooferEnabled = Config.Bind("Settings", "Color Code Spoofer", true, "Enable or disable the color code spoofer.");
+
+            isPlatformEnabled = PlatformCheckerEnabled.Value;
+            isVelocityEnabled = VelocityCheckerEnabled.Value;
+            isFPSEnabled = FPSCheckerEnabled.Value;
+            isColorCodeEnabled = ColorCodeSpooferEnabled.Value;
         }
     }
 }
