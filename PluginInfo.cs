8344c2b99f032eae30c8414efab36eb57135cbef
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-05-14 13:47:32 +0200

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -4,6 +4,6 @@
 	{
 		public const string GUID = "com.hansolo1000falcon.gorillatag.whoisthatmonke";
 		public const string Name = "WhoIsThatMonke";
-		public const string Version = "1.0.0";
+		public const string Version = "1.0.2";
 	}
 }
